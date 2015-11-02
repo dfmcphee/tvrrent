baba309ee61e181bd4df4cdd70b783ddb4f451f3
Dominic McPhee <dominic.mcphee@gmail.com>
2015-11-01 23:06:26 -0500

    Styling updates

--- src/client/components/Torrent.js
+++ src/client/components/Torrent.js
@@ -50,26 +50,23 @@ export default class Torrent extends React.Component {
     });
   }
 
-  render() {
-    let totals;
-    if (this.props.totalDownloaded) {
-      totals = this.formatDownloadTotal(this.props.totalDownloaded, this.props.size);
+  getTitle() {
+    let title;
+    if (this.state.editingTitle) {
+      title = (
+        <div className="input-with-addon">
+          <input value={this.state.title} onChange={::this.titleChanged} />
+          <button className="button" onClick={::this.updateTitle}>Update</button>
+        </div>
+      );
     }
-
-    let streamURL = `/stream?path=${encodeURIComponent(this.props.path)}`;
-    let playButton = <a className="button button--secondary" href={streamURL} target="_blank">Play <svg className="icon"><use xlinkHref="#play" /></svg></a>;
-
-    let download;
-    if (this.props.complete) {
-      download = <div className="torrent__actions">{playButton}</div>;
-    } else if (this.props.downloading) {
-      let percentage = <span className="torrent__totals">{totals}</span>;
-      let downloadButton = <button className="button" disabled="disabled"><svg className="icon"><use xlinkHref="#download" /></svg> {percentage}</button>;
-      download = <div className="torrent__actions">{downloadButton}{playButton}</div>
-    } else {
-      download = <div className="torrent__actions"><button className="button" onClick={::this.download}>Download <svg className="icon"><use xlinkHref="#download" /></svg></button></div>;
+    else {
+      title = <h3 onDoubleClick={::this.startEditing}>{this.state.title}</h3>;
     }
+    return title;
+  }
 
+  getPoster() {
     let image;
     if (this.props.poster) {
       let url = encodeURI(`/cover?url=${this.props.poster}`);
@@ -79,33 +76,88 @@ export default class Torrent extends React.Component {
 
       image = <div className="torrent__image" style={imageStyle}></div>;
     }
+    return image;
+  }
 
+  getActions() {
+    let download;
+    let streamURL = `/stream?path=${encodeURIComponent(this.props.path)}`;
+    let playButton = (
+      <a className="button button--secondary" href={streamURL} target="_blank">
+        <svg className="icon"><use xlinkHref="#play" /></svg>Play
+      </a>
+    );
+
+    if (this.props.complete) {
+      download = <div className="torrent__actions">{playButton}</div>;
+    }
+    else if (this.props.downloading) {
+      let percentage = <span className="torrent__totals">{this.getTotals()}</span>;
+      let downloadButton = (
+        <button className="button" onClick={::this.download}>
+          <svg className="icon"><use xlinkHref="#download" /></svg>{percentage}
+        </button>
+      );
+      download = (
+        <div className="torrent__actions">
+          {downloadButton}{playButton}
+        </div>
+      );
+    }
+    else {
+      download = (
+        <div className="torrent__actions">
+          <button className="button" onClick={::this.download}>
+            <svg className="icon"><use xlinkHref="#download" /></svg>Download
+          </button>
+        </div>
+      );
+    }
+
+    return download;
+  }
+
+  getTotals() {
+    let totals;
+    if (this.props.totalDownloaded) {
+      totals = this.formatDownloadTotal(this.props.totalDownloaded, this.props.size);
+    }
+    return totals;
+  }
+
+  getMeta() {
     let meta;
     if (this.props.show) {
       meta = <p>Season: {this.props.season} Episode: {this.props.episode}</p>;
-    } else {
+    }
+    else {
       meta = <p>{this.props.year}</p>;
     }
+    return meta;
+  }
 
-    let title;
-    if (this.state.editingTitle) {
-      title = (
-        <div className="input-with-addon">
-          <input value={this.state.title} onChange={::this.titleChanged} />
-          <button className="button" onClick={::this.updateTitle}>Update</button>
-        </div>
-      );
-    } else {
-      title = <h3 onDoubleClick={::this.startEditing}>{this.state.title}</h3>;
-    }
+  getPublished() {
+    return (
+      <p>
+        {moment(this.props.published).from(moment())}
+      </p>
+    )
+  }
+
+  render() {
+    let title = this.getTitle();
+    let actions = this.getActions();
+    let image = this.getPoster();
+    let meta = this.getMeta();
+    let published = this.getPublished();
 
     return (
       <div className="torrent" key={this.props.id}>
         {image}
         {title}
         {meta}
-        {download}
-        <p>{moment(this.props.published).from(moment())}</p>
+        {actions}
+        {published}
       </div>
     )
   }
